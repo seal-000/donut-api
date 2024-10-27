c03e2c84378401c41068e7b7038591e109587030
sleepyseal-000 <56409287+sleepyseal-000@users.noreply.github.com>
2024-10-26 20:04:51 -0400

    "move post"

--- app/api/donuts/[donutId]/route.ts
+++ app/api/donuts/[donutId]/route.ts
@@ -19,15 +19,6 @@ export async function GET(req: Request, context: any) {
 
 }
 
-// Path to JSON file
-const dataPath = path.join(process.cwd(), 'data.json');
 
-export async function POST(req : Request){
-    const newDonut = await req.json() //Parse 
-    const donutData: Data[] = JSON.parse(fs.readFileSync(dataPath, 'utf8')) //Read data
-    donutData.push(newDonut) // Push data
-    fs.writeFileSync(dataPath, JSON.stringify(donutData,  null, 2), 'utf8') // Write 
-    return NextResponse.json({message: "New donut added successfully"})
-}
 
 //TODO: Make method PUT and DELETE 
