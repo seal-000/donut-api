712d9129bb1740656506d2f8aea2e921d662c602
sleepyseal-000 <56409287+sleepyseal-000@users.noreply.github.com>
2024-11-25 18:08:36 -0500

    "put-updated and delete"

--- app/api/donuts/[donutId]/route.ts
+++ app/api/donuts/[donutId]/route.ts
@@ -1,8 +1,9 @@
 import data from "@/data.json"
 import { NextResponse } from "next/server"
-import { Data } from "@/types"
+import { Data, Donut } from "@/types"
 import fs from 'fs';
 import path from 'path';
+
 
 export async function GET(req: Request, context: any) {
     const { params } = context
@@ -20,5 +21,53 @@ export async function GET(req: Request, context: any) {
 }
 
 
+export async function PUT(req: Request, context: any) {
+    const { params } = context;
+    const donutId = params.donutId;
 
-//TODO: Make method PUT and DELETE 
+    const donutData: Data = data;
+    const donut = donutData.find((x) => donutId === x.id);
+
+    if (!donut) {
+        return NextResponse.json({ message: "Donut id not found" }, { status: 404 });
+    }
+
+    const updatedData = await req.json();
+    const updatedDonut: Donut = {
+        ...donut,
+        ...updatedData
+    };
+
+    const donutIndex = donutData.findIndex((x) => donutId === x.id);
+    donutData[donutIndex] = updatedDonut;
+
+    fs.writeFileSync(path.resolve('./data.json'), JSON.stringify(donutData, null, 2), 'utf8');
+
+    return NextResponse.json(updatedDonut);
+}
+
+
+
+export async function DELETE(req: Request, context: any) {
+    
+    const { params } = context;
+    const donutId = params.donutId;
+
+    const donutData: Data = data;
+    const donut = donutData.find((x) => donutId === x.id);
+
+    if (!donut) {
+        return NextResponse.json({ message: "Donut id not found" }, { status: 404 });
+    }
+
+    const donutIndex = donutData.findIndex((x) => donutId === x.id);
+    
+    //splice --> start, deleteCount
+    donutData.splice(donutIndex, 1);
+
+    fs.writeFileSync(path.resolve('./data.json'), JSON.stringify(donutData, null, 2), 'utf8');
+
+    return NextResponse.json({ message: "Donut deleted successfully" });
+
+}
+
